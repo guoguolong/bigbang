73aaccdf5b7c1b388563788a2c32d93cb15b4575
Koda <elon.guo@gmail.com>
2023-06-26 15:40:22 +0800

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hi Yosemite</h1>
+      <h1>Hi BigBang</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
