c80994e95bf6935fffcb0aba7458b74b7a61331a
Koda <elon.guo@gmail.com>
2023-07-20 09:43:13 +0800

    add tip to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hi BigBang</h1>
+      <h1>Hi BigBang - Demo the usage of Git Action</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
